5bbcc3de04950cc1b9a986852a2eb5262ba021ad
Juraev Ozodbek <ozodbekmcfly@gmail.com>
2021-04-23 15:35:29 +0500

    My aforizm is full

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import "./App.css";
 import Staffs from "./Components/Home/Staffs/Staffs";
 import Footer from "./Components/Home/Footer/Footer";
 import "./App.css";
+import Aforizms from './Components/Home/Aforizms/Aforizms';
+
 function App() {
   return (
     <div>
       <Staffs />
+      <Aforizms />
       <Footer />
     </div>
   );
--- src/Components/Home/Aforizms/Aforizms.js
+++ src/Components/Home/Aforizms/Aforizms.js
@@ -1,7 +1,31 @@
 import React from "react";
+import AforizmsCont from "./Container/AforizmsCont";
+import SchoolIcon from '@material-ui/icons/School';
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+import ImportContactsIcon from '@material-ui/icons/ImportContacts';
+
 
 function Aforizms() {
-  return <div>Aforizms</div>;
+  return <div className='aforizmContainer aforStyle-1' >
+    <div className='aforizmWrapper'>
+      
+      <div className='aforow row1'>
+        <SchoolIcon style={{color: '#51BE78'}} className='aforizmIcon' />
+        <AforizmsCont title='Our Philosphy' text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis recusandae, iure repellat quis delectus ea? Dolore, amet reprehenderit.' />
+      </div>
+
+      <div className='aforow row2'>
+        <ImportContactsIcon style={{color: '#51BE78'}} className='aforizmIcon' />
+        <AforizmsCont title='Academics Principle' text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis recusandae, iure repellat quis delectus ea? Dolore, amet reprehenderit.' />
+      </div>
+
+      <div className='aforow row3'>
+        <AccountBalanceIcon style={{color: '#51BE78'}} className='aforizmIcon' />
+        <AforizmsCont title='Key of Success' text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis recusandae, iure repellat quis delectus ea? Dolore, amet reprehenderit.' />
+      </div>
+
+    </div>
+  </div>;
 }
 
 export default Aforizms;
