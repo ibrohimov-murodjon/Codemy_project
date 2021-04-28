0fe683f1b819564a0b0110e7fd321a1793848362
galacticBlack <bekzodjontojiboyev8@gmail.com>
2021-04-28 14:47:43 +0500

    Akademics history from AboutUs page

--- src/Page/About/About.js
+++ src/Page/About/About.js
@@ -2,6 +2,7 @@ import React from "react";
 import Aforizms from "../../Components/Home/Aforizms/Aforizms";
 import Footer from "../../Components/Home/Footer/Footer";
 import TopImage from "../../Components/TopImage/TopImage";
+import AkademicsHistory from "../../Components/AboutUS/AkademicsHistory/AkdemicsHis";
 
 function About() {
   return (
@@ -13,6 +14,8 @@ function About() {
         lorem="Lorem ipsum dolor sit amet consectetur adipisicing."
       />
       {/* divImage */}
+      {/* menucha */}
+      <AkademicsHistory />
       {/* menucha */}
       {/* Why Academics Works */}
       <Aforizms />
