8bbbb60869bfe54c37373e44cb93c40a42350dc6
galacticBlack <bekzodjontojiboyev8@gmail.com>
2021-04-28 14:26:11 +0500

    Menu  Bar full boldi page ga caqiriliwga tayyor

--- src/Page/About/About.js
+++ src/Page/About/About.js
@@ -2,6 +2,7 @@ import React from "react";
 import Aforizms from "../../Components/Home/Aforizms/Aforizms";
 import Footer from "../../Components/Home/Footer/Footer";
 import TopImage from "../../Components/TopImage/TopImage";
+import MenuBar from "../../Components/MenuBar/MiddleMenu";
 
 function About() {
   return (
@@ -13,6 +14,8 @@ function About() {
         lorem="Lorem ipsum dolor sit amet consectetur adipisicing."
       />
       {/* divImage */}
+      {/* menucha */}
+      <MenuBar currentPath="About Us" />
       {/* menucha */}
       {/* Why Academics Works */}
       <Aforizms />
