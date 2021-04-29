de928203ab01bb60c061649008ab090ae417cd35
samadjon-04 <acc.falcon.0415@mail.ru>
2021-04-29 11:57:39 +0500

    v1.1.0 Videos Completed

--- src/Components/Home/Videos/Videos.js
+++ src/Components/Home/Videos/Videos.js
@@ -3,15 +3,18 @@ import "./Videos.css";
 import VideosModal from "./VideosModal";
 import { PlayArrow } from "@material-ui/icons";
 import { VideosData } from "./VideosData";
+import VideosAbout from './VideosAbout'
+import {Switch, Route} from "react-router-dom"
+// const [data, setData] = useState([])
 function Videos() {
-  const [data, setData] = useState("");
   const [show, setShow] = useState(false);
-
   const handleClick = () => {
     setShow(true);
-  };
+  };                                  
   return (
+    
     <div className="Videos">
+     
       <div className="Videos-News">
         <div className="Videos-head">
           <div className="Videos-News-link">
@@ -27,6 +30,7 @@ function Videos() {
                   className="Videos-news-img"
                 />
               </div>
+
               <h1 className="V-bottom-title">
                 <a href="#">JUNE 6, 2019 / ADMISSION, UPDATES</a>
               </h1>
@@ -36,61 +40,25 @@ function Videos() {
             </div>
             <div className="Videos-small">
               <div className="Small-news">
-                <div className="Videos-small-material">
-                  <div className="Videos-small-news">
-                    <span className="dark"></span>
-                    <img
-                      src="./images/blog_large_1.jpg"
-                      className="Videos-news-img"
-                    />
+                {VideosData.map((da, id) => (
+                  <div className="Videos-small-material">
+                    <div className="Videos-small-news">
+                      <span className="dark"></span>
+                      <img
+                        src={da.s_img_url}
+                        className="Videos-news-img"
+                      />
+                    </div>
+                    <div className="Videos-small-info">
+                      <h1 className="V-bottom-title">
+                        <a href="#">{da.s_data}</a>
+                      </h1>
+                      <a className="V-bottom-link" href="#">
+                        {da.s_img_title}
+                      </a>
+                    </div>
                   </div>
-                  <div className="Videos-small-info">
-                    <h1 className="V-bottom-title">
-                      <a href="#">JUNE 6, 2019 / ADMISSION, UPDATES</a>
-                    </h1>
-                    <a className="V-bottom-link" href="#">
-                      asdasda
-                    </a>
-                  </div>
-                </div>
-                <div className="Videos-small-material">
-                  <div className="Videos-small-news">
-                    <span className="dark"></span>
-                    <img
-                      src="./images/blog_large_1.jpg"
-                      className="Videos-news-img"
-                    />
-                    {/* <div>
-                    <h2 id="transition-modal-title">Transition modal</h2>
-                    <p id="transition-modal-description">react-transition-group animates me.</p>
-                  </div> */}
-                  </div>
-                  <div className="Videos-small-info">
-                    <h1 className="V-bottom-title">
-                      <a href="#">JUNE 6, 2019 / ADMISSION, UPDATES</a>
-                    </h1>
-                    <a className="V-bottom-link" href="#">
-                      asdasda
-                    </a>
-                  </div>
-                </div>
-                <div className="Videos-small-material">
-                  <div className="Videos-small-news">
-                    <span className="dark"></span>
-                    <img
-                      src="./images/blog_large_1.jpg"
-                      className="Videos-news-img"
-                    />
-                  </div>
-                  <div className="Videos-small-info">
-                    <h1 className="V-bottom-title">
-                      <a href="#">JUNE 6, 2019 / ADMISSION, UPDATES</a>
-                    </h1>
-                    <a className="V-bottom-link" href="#">
-                      asdasda
-                    </a>
-                  </div>
-                </div>
+                ))}
               </div>
             </div>
           </div>
@@ -104,17 +72,16 @@ function Videos() {
           </a>
         </div>
         <div className="players-box">
-          <div className="Video-player">
-            <img src="./images/blog_2.jpg" alt="" />
-            <PlayArrow className="video-player-icon" onClick={handleClick} />
-          </div>
-          <div className="Video-player">
-            <img src="./images/blog_2.jpg" alt="" />
-            <PlayArrow className="video-player-icon" onClick={handleClick} />
-          </div>
+          {VideosData.map((d, id) => (
+            <div className="Video-player" key={id}>
+              <img src={d.v_img_url} alt="" />
+              <PlayArrow className="video-player-icon" onClick={handleClick} />
+            </div>
+          ))}
         </div>
       </div>
       <VideosModal show={show} setShow={setShow} />
+      
     </div>
   );
 }
--- src/Components/Home/Videos/VideosData.js
+++ src/Components/Home/Videos/VideosData.js
@@ -1,6 +1,16 @@
 export const VideosData = [
   {
-    img: './images/blog_1.jpg',
-    url: ''
-  }
+    s_img_title: "asdasdasdas",
+    s_data: "JUNE 6, 2019 / ADMISSION, UPDATES",
+     v_img_url: "./images/hero_1.jpg",
+     s_img_url: "./images/blog_2.jpg",
+     videoUrl: "https://www.youtube.com/embed/hiG4kmPkCNo",
+   },
+   {
+    s_img_title: "asdasdasdas",
+    s_data: "JUNE 6, 2019 / ADMISSION, UPDATES",
+     s_img_url: "./images/course_3.jpg",
+     v_img_url: "./images/blog_1.jpg",
+     videoUrl: "https://www.youtube.com/embed/hiG4kmPkCNo",
+   },
 ]
