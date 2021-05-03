330d7972a8f419ee8ab1215ece711bcafcbf3f0a
jahongirmuminov <muminov707@gmail.com>
2021-05-03 23:43:45 +0500

    Admision Error

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Page/Home/Home";
 import About from "./Page/About/About";
 import Courses from "./Page/Courses/Courses";
 import { Route, Switch } from "react-router-dom";
+import Admission from "./Page/Admision/Admission";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/courses" component={Courses} />
+        <Route path="/admission" component={Admission} />
         {/* <Route path="/admission" component={Admission} />
         <Route path="/contact" component={Contact} /> */}
       </Switch>
