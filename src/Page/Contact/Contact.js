e129453f4bca9a2299100cf134e16dfcfe63f2a2
Xusrav01 <xmuxtorov0702@gmail.com>
2021-05-05 16:07:00 +0500

    mayda chuda uzgarishlar

--- src/Page/Contact/Contact.js
+++ src/Page/Contact/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 import Aforizms from "../../Components/Home/Aforizms/Aforizms";
 import Footer from "../../Components/Home/Footer/Footer";
 import TopImage from "../../Components/TopImage/TopImage";
-import OurTeachers from "../../Components/About/AboutTeachers/AboutTeachers";
+
 import MenuBar from "../../Components/MenuBar/MiddleMenu";
 
 import ContactForm from "../../Components/Contact/ContactForm";
@@ -23,7 +23,7 @@ function About() {
       <ContactForm />
 
       <Aforizms />
-      <OurTeachers />
+
       <Footer />
     </div>
   );
