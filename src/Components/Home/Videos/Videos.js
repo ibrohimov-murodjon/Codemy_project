f962498c64e95e9a19c5c1cbdb1405464e71bac6
galacticBlack <bekzodjontojiboyev8@gmail.com>
2021-05-24 15:53:55 +0500

    bitta error tozalandi

--- src/Components/Home/Videos/Videos.js
+++ src/Components/Home/Videos/Videos.js
@@ -2,44 +2,33 @@ import React, { useState, useEffect } from "react";
 import "./Videos.css";
 import VideosModal from "./VideosModal";
 import { PlayArrow } from "@material-ui/icons";
-import { SmallNews } from "./VideosData"
-import VideosAbout from './VideosAbout';
-import {Link } from "react-router-dom"
-import axios from "axios"
+import { SmallNews } from "./VideosData";
+import VideosAbout from "./VideosAbout";
+import { Link } from "react-router-dom";
+import axios from "axios";
 function Videos() {
   const [videosData, setVideosData] = useState([]);
   const [show, setShow] = useState(false);
-  const [oneD, setOneD] = useState([])
+  const [oneD, setOneD] = useState([]);
   const handleClick = (id) => {
-    setShow(true)
-    
-    
+    setShow(true);
+
     const eachVideos = videosData.filter((val) => {
       return val.id === id;
     });
-    setOneD(eachVideos[0])
+    setOneD(eachVideos[0]);
   };
-  const SmallClick = () => {
-
-  }
-
-
-
-
+  const SmallClick = () => {};
 
   useEffect(() => {
     axios.get("http://localhost:8080/api/videos").then((res) => {
       const fullVideos = res.data;
-      setVideosData(fullVideos)
-    })
-  }, [setVideosData])
-
-
-
+      setVideosData(fullVideos);
+    });
+  }, [setVideosData]);
 
   return (
     <div className="Videos">
-
       <div className="Videos-News">
         <div className="Videos-head">
           <div className="Videos-News-link">
@@ -50,10 +39,7 @@ function Videos() {
             <div className="Head-News">
               <div className="Videos-head-news">
                 <span className="dark"></span>
-                <img
-                  src="./images/bg_1.jpg"
-                  className="Videos-news-img"
-                />
+                <img src="./images/bg_1.jpg" className="Videos-news-img" />
               </div>
 
               <h1 className="V-bottom-title">
@@ -66,14 +52,14 @@ function Videos() {
             <div className="Videos-small">
               <div className="Small-news">
                 {SmallNews.map((da, id) => (
-                  <div className="Videos-small-material" onClick={() => SmallClick(da.id)}>
+                  <div
+                    className="Videos-small-material"
+                    onClick={() => SmallClick(da.id)}
+                  >
                     <Link to="/videosPage">
                       <div className="Videos-small-news">
                         <span className="dark"></span>
-                        <img
-                          src={da.s_img_url}
-                          className="Videos-news-img"
-                        />
+                        <img src={da.s_img_url} className="Videos-news-img" />
                       </div>
                     </Link>
                     <div className="Videos-small-info">
@@ -83,7 +69,6 @@ function Videos() {
                       <Link className="V-bottom-link" to="/videosPage">
                         {da.s_img_title}
                       </Link>
-
                     </div>
                   </div>
                 ))}
@@ -95,22 +80,23 @@ function Videos() {
       <div className="material-videos">
         <div className="Videos-News-link">
           <h1>Videodarslar</h1>
-          <a href="#" className>
-            View All Videos
-          </a>
+          <a href="#">View All Videos</a>
         </div>
         <div className="players-box">
           {videosData.map((d, id) => (
             <div className="Video-player" key={id}>
               <img src={d.img_url} alt="dsadas" />
-              <PlayArrow className="video-player-icon" onClick={()=>handleClick(d.id)} />
+              <PlayArrow
+                className="video-player-icon"
+                onClick={() => handleClick(d.id)}
+              />
             </div>
           ))}
         </div>
       </div>
       <VideosModal show={show} setShow={setShow} oneD={oneD.Url} />
-
-    </div>);
+    </div>
+  );
 }
 
 export default Videos;
